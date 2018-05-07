50dbffb9c3c27eca61968f0855b80eb1f5afcbf6
Unknown <theoleyenda1997@gmail.com>
2018-05-07 12:59:54 -0300

    Vercion de eintrega 2.0

--- TP2 -FPS/Juego/Assets/Assets/Scrips/Fantasma.cs
+++ TP2 -FPS/Juego/Assets/Assets/Scrips/Fantasma.cs
@@ -6,30 +6,84 @@ public class Fantasma : MonoBehaviour {
     public static int vidaFantasma = 30;
     public static int fantasmasEliminados = 0;
     private float tiempoMov = 0;
-    public float tiempoMovMax = 3;
+    private float tiempoMovMax = 4;
     private float randomX;
     private float randomZ;
     private float diley = 5;
+    private float diley1 = 5;
+    private float diley2 = 5;
+    private float diley3 = 5;
+    private float diley4 = 5;
     public static int puntosPorFantasma = 0;
     private int auxVidaFantasma = 30;
+    private bool entroPared1 = false;
+    private bool entroPared2 = false;
+    private bool entroPared3 = false;
+    private bool entroPared4 = false;
 	// Use this for initialization
 	void Start () {
-        tiempoMov = 3;
+        tiempoMov = 4;
         vidaFantasma = 30;
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        diley = diley + Time.deltaTime;
-        tiempoMov = tiempoMov + Time.deltaTime;
+        if (diley1 <= 5)
+        {
+            diley1 = diley1 + Time.deltaTime;
+        }
+        if (diley2 <= 5)
+        {
+            diley2 = diley2 + Time.deltaTime;
+        }
+        if (diley3 <= 5)
+        {
+            diley3 = diley3 + Time.deltaTime;
+        }
+        if (diley4 <= 5)
+        {
+            diley4 = diley4 + Time.deltaTime;
+        }
+        if (tiempoMov <= 4)
+        {
+            tiempoMov = tiempoMov + Time.deltaTime;
+        }
+        
         if(tiempoMov >= tiempoMovMax)
         {
-            randomX = Random.Range(-10, 10);
-            randomZ = Random.Range(-10, 10);
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(randomX, 0, randomZ), ForceMode.Acceleration);
+            randomX = Random.Range(-5, 5);
+            randomZ = Random.Range(-5, 5);
+            //gameObject.GetComponent<Rigidbody>().velocity = new Vector3(randomX, 0, randomZ);
+            tiempoMov = 0;
         }
-        if(auxVidaFantasma<= 0)
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(randomX, 0, randomZ);
+        if (entroPared1 && diley1> 5)
+        {
+            randomZ = randomZ * -1;
+            randomX = randomX * -1;
+            diley1 = 0;
+            
+        }
+        if (entroPared2 && diley2 > 5)
+        {
+            randomZ = randomZ * -1;
+            randomX = randomX * -1;
+            diley2 = 0;
+        }
+        if (entroPared3 && diley3 > 5)
+        {
+            randomZ = randomZ * -1;
+            randomX = randomX * -1;
+            diley3 = 0;
+        }
+        if (entroPared4 && diley4 > 5)
+        {
+            randomZ = randomZ * -1;
+            randomX = randomX * -1;
+            diley4 = 0;
+        }
+        if (auxVidaFantasma<= 0)
         {
             puntosPorFantasma = puntosPorFantasma + 200;
             fantasmasEliminados++;
@@ -49,6 +103,41 @@ public class Fantasma : MonoBehaviour {
             vidaFantasma = vidaFantasma - 10;
             auxVidaFantasma = auxVidaFantasma - 10;
             Debug.Log("vida Fantasma:" + vidaFantasma);
+        }
+        if(other.gameObject.tag == "pared1")
+        {
+            
+            entroPared1 = true;
+            entroPared2 = false;
+            entroPared3 = false;
+            entroPared4 = false;
+            
+            
+        }
+        if (other.gameObject.tag == "pared2")
+        {
+            
+            entroPared1 = false;
+            entroPared2 = true;
+            entroPared3 = false;
+            entroPared4 = false;
+            
+            
+        }
+        if (other.gameObject.tag == "pared3")
+        {
+            entroPared1 = false;
+            entroPared2 = false;
+            entroPared3 = true;
+            entroPared4 = false;
+        }
+        if (other.gameObject.tag == "pared4")
+        {
+            entroPared1 = false;
+            entroPared2 = false;
+            entroPared3 = false;
+            entroPared4 = true;
+            
         }
     }
 }
